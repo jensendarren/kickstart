ccb0054b4215b7eddf1bbc2aad3ab5990911e116
Darren Jensen <darren.jensen@gmail.com>
2019-09-10 16:13:14 +0700

    fix for update to metamask

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -2,11 +2,22 @@ import Web3 from 'web3';
 
 let web3;
 
-if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
-  // We are in the browser and metamask is running.
-  web3 = new Web3(window.web3.currentProvider);
+if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
+  console.log("We are in the browser and metamask is running...")
+
+  web3 = new Web3(window.ethereum);
+  window.addEventListener('load', async () => {
+      try {
+          // Request account access if needed
+          await window.ethereum.enable();
+      } catch (error) {
+          // User denied account access...
+          console.log("Why you deny?")
+      }
+  })
 } else {
-  // We are on the server *OR* the user is not running metamask
+  console.log("We are on the server *OR* the user is not running metamask");
+
   const provider = new Web3.providers.HttpProvider(
     'https://rinkeby.infura.io/orDImgKRzwNrVCDrAk5Q'
   );
